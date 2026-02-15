48ecabfca9aca8d0bf9a48807388ab39ac1d05e2
Rezaei <rezaeipow@gmail.com>
2026-02-15 14:04:59 +0330

    Sidebar and Topbar added to app

--- src/layout/ConsoleLayout/index.tsx
+++ src/layout/ConsoleLayout/index.tsx
@@ -10,9 +10,9 @@ export default function ConsoleLayout({
 }) {
   return (
     <div className="flex h-screen w-screen bg-background">
-      <Sidebar />
+      <Topbar />
       <div className="flex-1 flex flex-col">
-        <Topbar />
+        <Sidebar />
         <MainContent>{children}</MainContent>
       </div>
     </div>
