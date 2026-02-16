4d568c1f51c2da9b40b7b6ee92497fff1d2fc6ac
Rezaei <rezaeipow@gmail.com>
2026-02-16 22:24:53 +0330

    bug fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
-import { store } from "./app/store";
+import { store } from "./app/store/Index";
 import { ThemeProvider } from "./providers/ThemeProvider";
 import App from "./app/App";
+
+// MSW فقط در dev اجرا می‌شود
+if (import.meta.env.DEV) {
+  import("./mocks/browser").then(({ worker }) => {
+    worker.start();
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
